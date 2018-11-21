06762026cf469ead191380651d6017d90654cc8a
Octavio <lramirez@occ.com.mx>
2018-11-21 13:04:35 -0600

    programa terminado

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const URI = 'mongodb://localhost/mern-tasks';
  
-mongoose.connect(URI)
+mongoose.connect(URI, { useNewUrlParser: true })
 .then(db => console.log('DB is connected'))
 .catch(err => console.error(err));
 
